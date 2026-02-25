ecae3eb18ffe61af2281a4e596d625b344f47184
AGGD0001 <aggd0001@ce.pucmm.edu.do>
2026-02-24 20:17:40 -0400

    Fase beta, pero trabajando en los grafos

---
+++ DoveGAMMA_Code/src/main/Java/grafica/Launcher.java
@@ -0,0 +1,8 @@
+package grafica;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Esto "engaña" a Java para que arranque sin problemas
+        MainApp.main(args);
+    }
+}

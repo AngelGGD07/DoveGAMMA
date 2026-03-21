3634563f02a24c91fb54f8bc7e529d3a27b02f9a
aggd <aggd0001@ce.pucmm.edu.do>
2026-03-21 19:30:17 -0400

    Algo

--- DoveGAMMA_Code/src/main/java/grafica/AdaptadorVisual.java
+++ DoveGAMMA_Code/src/main/java/grafica/AdaptadorVisual.java
@@ -17,7 +17,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
+// nitido
 public class AdaptadorVisual {
 
     private static AdaptadorVisual instancia;
